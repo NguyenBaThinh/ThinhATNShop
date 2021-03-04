c016c92aa5b2daec5b90eac9b4fc9453b95bac7d
NguyenBaThinh <78465951+NguyenBaThinh@users.noreply.github.com>
2021-03-04 23:18:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,18 @@
   
 <?php
 session_start();
+	$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
+	$db_heroku = "d8k42dnhtd0o9i";
+	$user_heroku = "crmjpgdtqgprga";
+	$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
+	# Create connection to Heroku Postgres
+	$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+	$pg_heroku = pg_connect($conn_string);
+
+	if (!$pg_heroku)
+	{
+		die('Error: Could not connect: ' . pg_last_error());
+	}
 ?>
 <html>
 <head>
@@ -8,6 +20,38 @@ session_start();
 	<meta charset="utf-8">
 </head>
 <body>
+	
+	if (isset($_POST["btn_submit"])) 
+	{
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+	
+		$username = strip_tags($username);
+		$username = addslashes($username);
+		$password = strip_tags($password);
+		$password = addslashes($password);
+	
+	
+		if ($username == "" || $password =="") 
+		{
+			echo "username hoặc password bạn không được để trống!";
+		}
+		else
+		{
+			$sql = "select * from users where username = '$username' and password = '$password' ";
+			$query = mysqli_query($conn,$sql);
+			$num_rows = mysqli_num_rows($query);
+			if ($num_rows==0) 
+			{
+				echo "tên đăng nhập hoặc mật khẩu không đúng !";
+			}
+			else
+			{
+				$_SESSION['username'] = $username;
+				 header('Location: index.php');
+			}
+	}
+	
 	<form method="POST" action="login.php">
 	<fieldset>
 	    <legend>Đăng nhập</legend>

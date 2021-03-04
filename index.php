90cd5cae27ec38dec6622c5db190a9c43dff31df
NguyenBaThinh <78465951+NguyenBaThinh@users.noreply.github.com>
2021-03-05 06:39:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -56,25 +56,7 @@
 	<body>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<?php
-		if(isset($_GET['add'])){
-			$sql = "INSERT INTO atn set product_ID=:id, product_name=:name, product_stock=:stock, product_price=:price";
-			$result = pg_query($pg_heroku, $sql);
-			$id=htmlspecialchars(strip_tags($_POST['id']));
-			$name=htmlspecialchars(strip_tags($_POST['name']));
-       			$stock=htmlspecialchars(strip_tags($_POST['stock']));
-        		$price=htmlspecialchars(strip_tags($_POST['price']));
-			$result->bindParam(':id', $id);
-			$result->bindParam(':name', $name);
-       			$result->bindParam(':description', $stock);
-       			$result->bindParam(':price', $price);
-			if($result->execute())
-			{
-			  header('Location: index.php')
-			} 
-			echo "error"; #pg_last_error($pg_heroku)
-		}
-		?>
+		
 		<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
 		<table class='table table-hover table-responsive table-bordered'>
 			<tr>

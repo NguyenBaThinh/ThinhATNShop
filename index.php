a688c5a925c750d8b3bc3a57bd885a90dd126952
NguyenBaThinh <78465951+NguyenBaThinh@users.noreply.github.com>
2021-03-05 05:42:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -62,31 +62,17 @@
 			
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
+  	<form name ="input" method="get" action="" >
 		
-			<tr>
-			<td>product_id</td> 
-			<td><input type='number' name='id' class='form-control' /></td>
-			</tr>
-			<tr>
-			<td>product_name</td> 
-			<td><input type='number' name='name' class='form-control' /></td>
-			</tr>
-			<tr>
-			<td>product_stock</td> 
-			<td><input type='number' name='stock' class='form-control' /></td>
-			</tr>
-			<tr>
-			<td>product_price</td> 
-			<td><input type='number' name='price' class='form-control' /></td>
-			</tr>
-			<tr>
-			<td></td>
-			<td>
-				<input type="submit" value="Add" class='btn btn-primary'/>
-				<a href='index.php' class='btn btn-danger'>Cancel Go back</a>
-            		</td>
-       			 </tr>
+			
+			product_id: <input type='number' name='id' class='form-control' /><br />
+			product_name:<input type='number' name='name' class='form-control' /><br />
+			product_stock: <input type='number' name='stock' class='form-control' /><br />
+			product_price: <input type='number' name='price' class='form-control' /><br />
+			
+				<input type="submit" value="Add" class='btn btn-primary'/><br />
+				<a href='index.php' class='btn btn-danger'>Cancel Go back</a><br />
+            		
 			
 		</form>
 

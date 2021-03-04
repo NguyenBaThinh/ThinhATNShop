25a7c5d794bcda74c8ca974d2bd7af2c1b3a6899
NguyenBaThinh <78465951+NguyenBaThinh@users.noreply.github.com>
2021-03-05 06:43:09 +0700

    Update create.php

--- create.php
+++ create.php
@@ -1,21 +1,17 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
-      
-    <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-          
-</head>
-<body>
-  
-    <!-- container -->
-    <div class="container">
-   
-        <div class="page-header">
-            <h1>Create Product</h1>
-        </div>
-    <?php
+<?php
+		$host_heroku = "ec2-35-171-57-132.compute-1.amazonaws.com";
+		$db_heroku = "d8847oi5c0tnc2";
+		$user_heroku = "jvkzuhcsezvfte";
+		$pw_heroku = "b7c215aa5cb23f04f16204e65d25bb5258955ca280199c68aff540d9fbba4b71";
+		# Create connection to Heroku Postgres
+		$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+		$pg_heroku = pg_connect($conn_string);
+
+		if (!$pg_heroku)
+		{
+		Exit('Error: Could not connect: ' . pg_last_error());
+		}
+	    
 		if(isset($_POST['add'])){
 			$sql = "INSERT INTO atn set product_ID=:id, product_name=:name, product_stock=:stock, product_price=:price";
 			$result = pg_query($pg_heroku, $sql);
@@ -34,6 +30,24 @@
 			echo "error"; #pg_last_error($pg_heroku)
 		}
 		?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+      
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+          
+</head>
+<body>
+  
+    <!-- container -->
+    <div class="container">
+   
+        <div class="page-header">
+            <h1>Create Product</h1>
+        </div>
+    
       
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <table class='table table-hover table-responsive table-bordered'>

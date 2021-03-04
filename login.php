00f2ce4e116966f0316f498ef5fd8283021db370
NguyenBaThinh <78465951+NguyenBaThinh@users.noreply.github.com>
2021-03-04 23:34:03 +0700

    Update login.php

--- login.php
+++ login.php
@@ -11,18 +11,18 @@ session_start();
 <body>
 <?php
 	$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
-			$db_heroku = "d8k42dnhtd0o9i";
-			$user_heroku = "crmjpgdtqgprga";
-			$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			$pg_heroku = pg_connect($conn_string);
-			
-			if (!$pg_heroku)
-			{
-				die('Error: Could not connect: ' . pg_last_error());
-			}
-	
+	$db_heroku = "d8k42dnhtd0o9i";
+	$user_heroku = "crmjpgdtqgprga";
+	$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
+	# Create connection to Heroku Postgres
+	$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+	$pg_heroku = pg_connect($conn_string);
+
+	if (!$pg_heroku)
+	{
+		die('Error: Could not connect: ' . pg_last_error());
+	}
+
 
 	if (isset($_POST["btn_submit"])) 
 	{
@@ -61,17 +61,17 @@ session_start();
 	    	<table>
 	    		<tr>
 	    			<td>Username</td>
-	    			<td><input type="text" name="username" size="30"></td>
+	    			<td><input type="text" name="username" size="30" required></td>
 	    		</tr>
 	    		<tr>
 	    			<td>Password</td>
-	    			<td><input type="password" name="password" size="30"></td>
+	    			<td><input type="password" name="password" size="30" required></td>
 	    		</tr>
 	    		<tr>
 	    			<td colspan="2" align="center"> <input type="submit" name="btn_submit" value="Đăng nhập"></td>
 	    		</tr>
 	    	</table>
-  </fieldset>
-  </form>
+  	</fieldset>
+ 	 </form>
 </body>
 </html>
